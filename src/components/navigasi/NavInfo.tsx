ef9db1793a757aecd47bd67d01c5829293ce4692
Wahyudi Umar <wahyucherryorlove@gmail.com>
2024-03-11 12:46:45 +0700

    Merubah style untuk navbar info

--- src/components/navigasi/NavInfo.tsx
+++ src/components/navigasi/NavInfo.tsx
@@ -9,16 +9,25 @@ export function NavInfo() {
     <div className="h-[38%] flex items-center justify-end px-14">
       <div className=""></div>
       <div className="flex items-center gap-x-7">
-        <Link href="" className="text-xs flex gap-x-2 items-center">
-          <GmailIcons className="w-4 h-4" />
+        <Link
+          href=""
+          className="text-xs flex gap-x-2 text-neutral-500 items-center"
+        >
+          <GmailIcons className="w-4 h-4 fill-neutral-500" />
           wahyucherryorlove@gmail.com
         </Link>
-        <Link href="" className="text-xs flex gap-x-2 items-center">
-          <PhoneIcons className="w-4 h-4" />
+        <Link
+          href=""
+          className="text-xs flex gap-x-2 text-neutral-500 items-center"
+        >
+          <PhoneIcons className="w-4 h-4 fill-neutral-500" />
           +62 821 9482 3066
         </Link>
-        <Link href="" className="text-xs flex gap-x-2 items-center">
-          <TranslateIcons className="w-4 h-4" />
+        <Link
+          href=""
+          className="text-xs flex gap-x-2 text-neutral-500 items-center"
+        >
+          <TranslateIcons className="w-4 h-4 fill-neutral-500" />
           Bahasa Indonesia
         </Link>
       </div>
